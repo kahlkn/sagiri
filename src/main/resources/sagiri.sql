4dc09c5d0e32643a73f6ea83e2f5e73da561aef0
Kahle <kahlkn@gmail.com>
2020-01-06 19:22:54 +0800

    Update0106

--- src/main/resources/sagiri.sql
+++ src/main/resources/sagiri.sql
@@ -92,13 +92,14 @@ CREATE TABLE `t_event_record` (
   `visitor_id` varchar(32) DEFAULT '' COMMENT '访问者ID',
   `user_id` varchar(32) DEFAULT '' COMMENT '用户ID',
   `time` datetime DEFAULT NULL COMMENT '事件时间',
+  `client_Id` varchar(32) DEFAULT '' COMMENT '客户端ID',
   `name` varchar(50) DEFAULT '' COMMENT '事件名称',
   `code` varchar(50) DEFAULT '' COMMENT '事件Code',
   `type` varchar(20) DEFAULT '' COMMENT '事件类型',
   `level` int(8) DEFAULT '0' COMMENT '事件级别',
-  `data` text COMMENT '事件数据',
-  `provider` varchar(80) DEFAULT '' COMMENT '事件的提供者',
   `description` varchar(500) DEFAULT '' COMMENT '事件描述',
+  `variables` text COMMENT '事件变量',
+  `provider` varchar(80) DEFAULT '' COMMENT '事件的提供者',
   `create_user` varchar(32) DEFAULT 'system' COMMENT '创建者',
   `create_date` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_user` varchar(32) DEFAULT 'system' COMMENT '修改者',
@@ -108,12 +109,15 @@ CREATE TABLE `t_event_record` (
   KEY `idx_visitor_id` (`visitor_id`) USING BTREE,
   KEY `idx_user_id` (`user_id`) USING BTREE,
   KEY `idx_time` (`time`) USING BTREE,
+  KEY `idx_client_Id` (`client_Id`) USING BTREE,
   KEY `idx_name` (`name`) USING BTREE,
   KEY `idx_code` (`code`) USING BTREE,
   KEY `idx_type` (`type`) USING BTREE,
   KEY `idx_level` (`level`) USING BTREE,
   KEY `idx_provider` (`provider`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='事件记录表';
+
+
 
 
 
