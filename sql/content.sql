57128bb475fe3aaf61ff466a288d173d9d29ea0c
Kahle <kahlkn@gmail.com>
2021-03-17 00:59:41 +0800

    Update0317

--- sql/content.sql
+++ sql/content.sql
@@ -2,7 +2,7 @@
 
 DROP TABLE IF EXISTS `t_article_category`;
 CREATE TABLE `t_article_category` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '文章分类ID',
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '分类ID',
   `parent_id` bigint(20) DEFAULT NULL COMMENT '父分类ID',
   `name` varchar(50) NOT NULL COMMENT '分类名称',
   `description` varchar(200) NOT NULL DEFAULT '' COMMENT '分类描述',
@@ -15,6 +15,22 @@ CREATE TABLE `t_article_category` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10001001 DEFAULT CHARSET=utf8mb4 COMMENT='文章分类表';
 INSERT INTO `t_article_category` VALUES (10001001, NULL, '未分类', '', 0, 'system', '2021-02-25 00:00:01', 'system', '2021-02-25 00:00:01', 1);
+
+
+DROP TABLE IF EXISTS `t_article_tag`;
+CREATE TABLE `t_article_tag` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '标签ID',
+  `name` varchar(50) NOT NULL COMMENT '标签名称',
+  `slug` varchar(100) NOT NULL DEFAULT '' COMMENT '自定义访问路径',
+  `description` varchar(200) NOT NULL DEFAULT '' COMMENT '标签描述',
+  `sort` int(6) NOT NULL DEFAULT '0' COMMENT '排序',
+  `creator_id` varchar(32) NOT NULL COMMENT '创建者ID',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `updater_id` varchar(32) NOT NULL COMMENT '更新者ID',
+  `update_time` datetime NOT NULL COMMENT '更新时间',
+  `alive_flag` tinyint(4) NOT NULL DEFAULT '1' COMMENT '数据状态',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10001001 DEFAULT CHARSET=utf8mb4 COMMENT='文章标签表';
 
 
 DROP TABLE IF EXISTS `t_article_relation`;
@@ -31,7 +47,8 @@ CREATE TABLE `t_article_relation` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='文章关系表';
 
-// https://en.wikipedia.org/wiki/Clean_URL#Slug
+
+-- https://en.wikipedia.org/wiki/Clean_URL#Slug
 DROP TABLE IF EXISTS `t_article`;
 CREATE TABLE `t_article` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '文章ID',
